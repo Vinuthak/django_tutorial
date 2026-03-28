be4841fabb10d6aef431f2a4d3eac9b71b38da31
Vinuthak <vinutha.kasaragod@gmail.com>
2026-03-28 20:51:00 +0100

    Add URLs and views to the app - challenges under monthly-challenge project

---
+++ challenges/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
+
+monthly_challenges = {
+    "january": "january challenge",
+    "february": "february challenge",
+    "march": "march challenge",
+    "april": "april challenge",
+    "may": "may challenge",
+    "june": "june challenge",
+    "july": "july challenge",
+    "august": "august challenge",
+    "september": "september challenge",
+    "october": "october challenge",
+    "november": "november challenge",
+    "december": "december challenge",
+}
+
+
+# Create your views here.
+def monthly_challenge_by_string(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+    except:
+        return HttpResponseNotFound("<h3>Not allowed for this month!</h3>")
+    return HttpResponse(f"<h3>{challenge_text}</h3>")
+
+
+def monthly_challenge_by_num(request, month):
+    months = list(monthly_challenges.keys())
+    if month > len(months):
+        return HttpResponseNotFound("Invalid month number!")
+    redirect_month = months[month - 1]
+    redirect_path = reverse("month-challenge", args=[redirect_month])
+    return HttpResponseRedirect(redirect_path)
